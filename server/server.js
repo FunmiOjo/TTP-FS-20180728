2a497f83712f9e0c09063e4e51667edde2bd6694
Funmi Ojo <oluwafunmi.ojo@gmail.com>
2018-11-29 17:23:52 -0500

    Server setup

---
+++ server/server.js
@@ -0,0 +1,46 @@
+const path = require('path')
+const express = require('express')
+const morgan = require('morgan')
+const bodyParser = require('body-parser')
+const db = require('./database')
+const session = require('express-session')
+const SequelizeStore = require('connect-session-sequelize')(session.Store)
+const dbStore = new SequelizeStore({ db: db })
+const passport = require('passport')
+const app = express()
+
+//middleware
+dbStore.sync()
+app.use(session({
+  secret: process.env.SESSION_SECRET || 'yoursecretlovewhycantwetellsomebody',
+  store: dbStore,
+  resave: false,
+  saveUninitialized: false
+}))
+
+app.use(passport.initialize())
+app.use(passport.session())
+
+app.use(morgan('dev'))
+app.use(express.static(path.join(__dirname, '..', '/public')))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+//routes
+app.use('/api', require('./api'))
+app.use('auth', require('./auth'))
+
+app.get('*', (req, res, next) => {
+  res.sendFile(path.join(__dirname, '..', '/public/index.html'))
+})
+
+app.use((err, req, res, next) => {
+  console.error(err)
+  console.error(err.stack)
+  res.status(err.status || 500).send(err.message || 'Internal server error')
+})
+
+module.exports = app
+
+
+
