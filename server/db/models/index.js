7e5474bcc625d7142bd6472c0588a49156c33f8a
Funmi Ojo <oluwafunmi.ojo@gmail.com>
2018-12-05 11:34:37 -0500

    Exported Trade model

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -1,8 +1,10 @@
-const Stock = require('./stock')
-const User = require('./user')
 const Balance = require('./balance')
+const Stock = require('./stock')
+const Trade = require('./trade')
+const User = require('./user')
 
 Balance.belongsTo(User)
 Stock.belongsTo(User)
+Trade.belongsTo(User)
 
 module.exports = { Balance, Stock, User }
