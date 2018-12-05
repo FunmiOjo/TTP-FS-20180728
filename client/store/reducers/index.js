5045f2fd5f0249acc23358774e26952216d226b4
Funmi Ojo <oluwafunmi.ojo@gmail.com>
2018-12-05 12:26:25 -0500

    Added trade store

--- client/store/reducers/index.js
+++ client/store/reducers/index.js
@@ -2,11 +2,13 @@ import { combineReducers } from 'redux'
 import balance from './balance'
 import user from './user'
 import stock from './stock'
+import trade from './trade'
 
 const reducer = combineReducers({
   balance,
   user,
   stock,
+  trade,
 })
 
 export default reducer
---
+++ client/store/reducers/trade.js
@@ -0,0 +1,42 @@
+import axios from 'axios'
+
+// action types
+const SET_ADDED_TRADE = 'SET_ADDED_TRADE'
+
+// action creators
+const setAddedTrade = trade => {
+  return {
+    type: SET_ADDED_TRADE,
+    trade,
+  }
+}
+
+// thunk creators
+export const addTrade = trade => {
+  return async dispatch => {
+    try {
+      const addedTrade = await axios.post('/api/trades', trade)
+      dispatch(setAddedTrade(addedTrade))
+    } catch (error) {
+      console.error(error)
+    }
+  }
+}
+
+// reducer
+const initialState = {
+  trades: [],
+}
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case SET_ADDED_TRADE:
+      return {
+        ...state,
+        trades: [...state.trades, action.trade],
+      }
+    default:
+      return state
+  }
+}
+
+export default reducer
